7a2dd17ef4b33c0a06ea59815a801f6112de9644
Noah Seltzer <noahseltzer@outlook.com>
2025-08-02 12:18:23 -0700

    init:init

---
+++ tiny-pass/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("\"Press + to add a new item, any other key to search available secrets \"");
+
+var key = Console.ReadKey(true);
+if (key.KeyChar == '+') {
+    Console.WriteLine("Enter secret name + ENTER");
+    var name = Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine($"Enter the secret value + ENTER");
+    var value = Console.ReadLine();
+    
+    
+    
+    
+}
+Console.WriteLine($"got: {key.Key}");
+
+
